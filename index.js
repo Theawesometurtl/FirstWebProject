dd679304d1bbf99ef91243ec072be30a1cb5e650
Theawesometurtl <95330177+Theawesometurtl@users.noreply.github.com>
2022-04-18 13:01:08 -0700

    hi

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const canvas = document.querySelector('canvas')
+//const canvas = document.querySelector('canvas')
+//const c = canvas.getContext('2d')
+
+function triangleNumDivisors(amount) {
+   var currentTriangleNum = amount * 2
+   while (divisorsFound < amount) {
+      divisorsFound = 0
+      tryUpTo = Math.ceil((currentTriangleNum / 2) + 2)
+      for (let divisor = 1; divisor < tryUpTo; divisor++) {
+         if (currentTriangleNum % divisor === 0) {
+            divisorsFound++
+         }
+      }
+      currentTriangleNum++
+      Console.log(currentTriangleNum, divisorsFound)
+   }
+   document.getElementById("answer").innerHTML = currentTriangleNum;
+}
+
+
+//canvas.width = innerWidth
+//canvas.height = innerHeight
 
 
 
-
-canvas.width = innerWidth
-canvas.height = innerHeight
+//class Player {
+   
+//}
